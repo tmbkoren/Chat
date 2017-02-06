c04348b116b2440670038350a54126b3dd1a6836
vladkoren <vladkorenevsky@gmail.com>
2017-02-06 20:49:02 +0200

    Client update
    Added "client" class

--- src/Client.java
+++ src/Client.java
@@ -2,9 +2,6 @@ import java.net.Socket;
 
 public class Client {
 	Socket socket;
-	public static void main(String[] args){
-		
-	}
 	Client(String ipAddress, int port){
 		try {
 			socket = new Socket(ipAddress, port);
--- src/StartGUI.java
+++ src/StartGUI.java
@@ -1,10 +1,17 @@
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class StartGUI {
 
 	private JFrame frame;
+	private JTextField ipTextField;
+	private JTextField portTextField;
 
 	/**
 	 * Launch the application.
@@ -36,6 +43,34 @@ public class StartGUI {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblIp = new JLabel("IP");
+		lblIp.setBounds(100, 70, 46, 14);
+		frame.getContentPane().add(lblIp);
+		
+		ipTextField = new JTextField();
+		ipTextField.setBounds(156, 67, 86, 20);
+		frame.getContentPane().add(ipTextField);
+		ipTextField.setColumns(10);
+		
+		JLabel lblPort = new JLabel("Port");
+		lblPort.setBounds(100, 120, 46, 14);
+		frame.getContentPane().add(lblPort);
+		
+		portTextField = new JTextField();
+		portTextField.setBounds(156, 117, 86, 20);
+		frame.getContentPane().add(portTextField);
+		portTextField.setColumns(10);
+		
+		JButton btnConnect = new JButton("Connect!");
+		btnConnect.setBounds(153, 173, 89, 23);
+		frame.getContentPane().add(btnConnect);
+		btnConnect.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				Client client = new Client(ipTextField.getText(), Integer.parseInt(portTextField.getText()));
+			}
+		});
 	}
 
 }
